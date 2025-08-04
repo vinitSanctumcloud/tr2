0cf09eda95101f2f0f5ef18fd431ad7d5c2e2dc4
vinitSanctumcloud <vinit@sanctumcloud.com>
2025-08-04 17:18:28 +0530

    dddsdsd

--- src/components/aiagent.tsx
+++ src/components/aiagent.tsx
@@ -162,7 +162,7 @@ export function AiAgent({
                 />
               ) : (
                 <img
-                  src={agentDetails?.greeting_media_url ?? 'https://via.placeholder.com/150'}
+                  src={agentDetails?.greeting_media_url ?? 'https://plus.unsplash.com/premium_photo-1751107029838-c341391ea8b2?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
                   alt={agentDetails?.agent_name ?? 'Agent Avatar'}
                   className="w-full h-full object-cover object-center"
                 />
